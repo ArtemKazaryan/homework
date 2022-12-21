178088cd9eef8b73e4fcacb5d543751e9b047de6
Artem Kazaryan <mirony4@yandex.ru>
2022-12-21 15:29:51 +0300

    added validParentheses function

---
+++ js/index.js
@@ -0,0 +1,26 @@
+function validParentheses(parents) {
+    let counter = 0;
+
+    for (let i = 0; i < parents.length; i++) {
+        if (parents[i] === "(") {
+            counter++;
+        } else {
+            counter--;
+        }
+        if (counter < 0) {
+            return false;
+        }
+    }
+    
+    if (counter === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+ 
+
+validParentheses("()"); // true
+validParentheses(")(()))"); // false
+validParentheses("("); // false
+validParentheses("(())((()())())"); // true
